726a0b39a1ab4db41cf7539a9d1b7ec74f0d5283
Dark Samurai <keshav223.keshav@gmail.com>
2025-02-19 23:11:55 +0530

    Update getHappyString.py

--- getHappyString.py
+++ getHappyString.py
@@ -1,12 +1,27 @@
 n = 3
 k = 9
-from itertools import permutations as p
+
 def getHappyString(n, k):
-    string = ""
-    mx = 3 * 2 ** (n-1)
-    res = []
-    for i in range(mx):
-        freeze = ""
-        if 
+        total = 2 ** (n - 1) 
+        if k > 3 * total:
+            return "" 
+        
+        chars = ['a', 'b', 'c']
+        result = []
+        index = (k - 1) // total 
+        result.append(chars[index])
+        k = (k - 1) % total + 1
+
+        for i in range(1, n):
+            total //= 2
+            for ch in chars:
+                if ch != result[-1]:
+                    if k > total:
+                        k -= total
+                    else:
+                        result.append(ch)
+                        break
+
+        return "".join(result)
         
 getHappyString(n, k)
